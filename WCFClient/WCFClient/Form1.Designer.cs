eb0648ab51c8575f28cffb735e332c7589fb3dd8
guoxuri <853523391@qq.com>
2016-01-23 00:02:50 +0800

    完成基本的功能，待完善

---
+++ WCFClient/WCFClient/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WCFClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdda = new System.Windows.Forms.TextBox();
+            this.txtAddb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "被加数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "加数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "服务端回调的消息：";
+            // 
+            // btnSend
+            // 
+            this.btnSend.AutoSize = true;
+            this.btnSend.Location = new System.Drawing.Point(28, 150);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(99, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "调用基础服务端";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "[消息]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "结果：";
+            // 
+            // txtAdda
+            // 
+            this.txtAdda.Location = new System.Drawing.Point(85, 26);
+            this.txtAdda.Name = "txtAdda";
+            this.txtAdda.Size = new System.Drawing.Size(100, 21);
+            this.txtAdda.TabIndex = 6;
+            // 
+            // txtAddb
+            // 
+            this.txtAddb.Location = new System.Drawing.Point(85, 70);
+            this.txtAddb.Name = "txtAddb";
+            this.txtAddb.Size = new System.Drawing.Size(100, 21);
+            this.txtAddb.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "[num]";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAddb);
+            this.Controls.Add(this.txtAdda);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "MainForm";
+            this.Text = "客户端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdda;
+        private System.Windows.Forms.TextBox txtAddb;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WCFClient/WCFClient/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WCFClient
+{
+    public partial class MainForm : Form, CallbacksServiceReference.ICallbacksServiceCallback, IDisposable
+    {
+        /// <summary>
+        /// 基础服务的客户端类
+        /// </summary>
+        BaseServiceReference.BaseServiceClient baseServiceClient;
+        /// <summary>
+        /// 服务器回调的客户端类
+        /// </summary>
+        CallbacksServiceReference.CallbacksServiceClient callbacksServiceClient;
+        public MainForm()
+        {
+            InitializeComponent();
+            baseServiceClient = new BaseServiceReference.BaseServiceClient();   //初始化基础服务客户端类
+            callbacksServiceClient = new CallbacksServiceReference.CallbacksServiceClient(new InstanceContext(this));   //初始化服务器回调客户端类
+            RegCallBack();
+        }
+
+        public void NotifyClientMsg(string msg)
+        {
+            label4.Text = msg;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            int a = 0, b = 0;
+            if (!int.TryParse(txtAdda.Text, out a))
+            {
+                MessageBox.Show("请填写正确格式的被加数。");
+            }
+            if (!int.TryParse(txtAddb.Text, out b))
+            {
+                MessageBox.Show("请填写正确格式的加数。");
+            }
+            label6.Text = baseServiceClient.Addition(a, b).ToString();
+        }
+
+        /// <summary>
+        /// 向服务端注册回调事件。
+        /// 即监听服务端向客户端发送的命令。
+        /// </summary>
+        private void RegCallBack()
+        {
+            try
+            {
+                callbacksServiceClient.MonitorServer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        new void Dispose()
+        {
+            try
+            {
+                //callbacksServiceClient.UnMonitorServer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
